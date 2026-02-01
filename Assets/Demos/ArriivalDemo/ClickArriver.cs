b33e4068f8d291d9161b8a99b915424dc8fd254d
professorK <jeffpk@gmail.com>
2026-02-01 14:00:52 -0500

    arrive behavior implemented
    Now has arrive behavior and demo

---
+++ Assets/Demos/ArriivalDemo/ClickArriver.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickArriver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Arriver arriver;
+    private Breaker breaker;
+    public int decelerationConstant = 1;
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        SteeringObject so = GetComponent<SteeringObject>();
+        arriver = new Arriver(rb,decelerationConstant);
+        so.AddSteeringBehavior(arriver);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(
+                Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                arriver.SetArrivalPosition(hit.point);
+            }
+        }
+        
+    }
+}
---
+++ Assets/SteeringBehaviors/Arrive.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arriver: SteeringBehavior {
+    Vector3 targetPosition;
+    bool unset = true;
+    Rigidbody rb;
+    int decelerationConstant;
+    private float tweaker = 1.7f;
+	
+    public Arriver(Rigidbody rb,int decelerationConstant )
+    {
+        this.rb = rb;
+        this.decelerationConstant = decelerationConstant;
+    }
+
+    public void SetArrivalPosition(Vector3 targetPosition)
+    {
+            this.targetPosition = targetPosition;
+            unset = false;
+    }
+
+    // ReSharper disable Unity.PerformanceAnalysis
+    public Vector3 CalculateSteeringForce(float maxVelocity)
+    {
+        Debug.Log("Enter Calculating arrival force");
+        if (!unset)
+        {
+            Debug.Log("Calculating arrival force");
+            Vector3 toTarget = targetPosition - rb.position;
+            float distance = toTarget.magnitude;
+            if (distance>0){
+                float speed = distance/(decelerationConstant*tweaker);
+                speed = speed>maxVelocity?maxVelocity:speed;
+                Vector3 desiredVelocity = toTarget  * speed / distance;
+                Vector3 result = desiredVelocity - rb.velocity;
+                Debug.Log("Force = " + result.ToString() + "");
+                return result;
+            } else {
+                return new Vector3(0, 0, 0);
+            }
+        }
+        else
+        {
+            //no force}
+            return new Vector3(0, 0, 0);
+        }
+    }
+
+}
